cc82cf39972dc26649ae8ea35a14baf4fab4a879
kevinash <esselebomebara@gmail.com>
2024-09-29 16:12:29 +0200

    initialisation service user query

---
+++ fendex-app/fdx-user-query-service/src/test/java/com/fendex/user/query/FdxUserQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fendex.user.query;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdxUserQueryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
