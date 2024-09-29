097e19fdd801824c62523dc4f7b40b9451d46939
kevinash <esselebomebara@gmail.com>
2024-09-29 15:05:02 +0200

    initialisation des services gateway et user command

---
+++ fendex-app/fdx-gateway-service/src/test/java/com/fendex/gateway/FdxGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fendex.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdxGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ fendex-app/fdx-user-command-service/src/main/java/com/fendex/user/command/FdxUserCommandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fendex.user.command;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdxUserCommandServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FdxUserCommandServiceApplication.class, args);
+    }
+
+}
