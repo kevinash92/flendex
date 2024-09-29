62e130f69f47e312261eeaccee3fd3272c04be04
kevinash <esselebomebara@gmail.com>
2024-09-29 12:15:26 +0200

    initialisation du service fdx-discovery-service

---
+++ fendex-app/fdx-discovery-service/src/test/java/com/fendex/discovery/FdxDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fendex.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdxDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
